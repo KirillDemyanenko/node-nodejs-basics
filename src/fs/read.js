4af92543bae4c6ece72b5235925bed82e6c36c82
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-13 22:22:34 +0300

    refactor: fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -2,7 +2,8 @@ import { readFile } from 'node:fs'
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url';
 const read = async () => {
-    readFile(resolve(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRead.txt'), (err, data) => {
+    readFile(resolve(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRead.txt'),
+        (err, data) => {
         if (err) throw new Error('FS operation failed')
         console.log(data.toString())
     })
