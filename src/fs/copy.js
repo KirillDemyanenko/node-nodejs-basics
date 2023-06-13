171f063254657aa42e51a701f7baf82ada8b02e4
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-13 20:14:20 +0300

    feat: fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import { cp } from 'node:fs'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const copy = async () => {
-    // Write your code here 
+    cp(__dirname.concat('\\files\\'),
+        __dirname.concat('\\files_copy\\'),
+        { recursive: true, errorOnExist: true, force: false },
+        (err) => {
+            if (err) throw new Error('FS operation failed')
+        })
 };
 
 await copy();
