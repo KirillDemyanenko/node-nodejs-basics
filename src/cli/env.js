61f6c91d307694c6cc75851fc430029c0a45fef9
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-13 21:24:29 +0300

    feat: cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const variables = Object.entries(process.env).filter(value =>
+        value[0].indexOf('RSS_') === 0).map(val => (`${val[0]}=${val[1]};`))
+    console.log(variables.join(' ').split('').slice(0, -1).join(''))
 };
 
 parseEnv();
