ef110d297a3e4f4e8c872da60c95d90a7ad9e76e
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-13 17:47:00 +0300

    feat: cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    const result = []
+    let str = ''
+    for (let i = 0; i < args.length; i += 2) {
+        result.push([args[i],args[i + 1]])
+    }
+    result.forEach(value => str = str.concat(value[0].slice(2, value[0].length), ' is ', value[1], ', '))
+    console.log(str.slice(0, -2))
 };
 
 parseArgs();
