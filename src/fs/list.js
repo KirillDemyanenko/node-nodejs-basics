fbc7b1548e36e30c68702b7dc4d28f8c92368690
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-13 20:48:26 +0300

    fix: fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,6 +1,8 @@
 import { readdir } from 'node:fs'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
 const list = async () => {
-    readdir('./files', (err, files) => {
+    readdir(resolve(dirname(fileURLToPath(import.meta.url)), 'files'), (err, files) => {
         if (err) throw new Error('FS operation failed')
         console.log(...files)
     })
