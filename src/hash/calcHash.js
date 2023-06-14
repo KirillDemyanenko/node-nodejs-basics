d797f2caaf5b478a1aae9d07c98587b607b61e24
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-14 18:42:59 +0300

    feat: hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256')
+    let dataForHash = ''
+    readFile(resolve(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToCalculateHashFor.txt'),
+        (err, data) => {
+            if (err) throw new Error('FS operation failed')
+            dataForHash.concat(data.toString())
+        })
+    hash.update(dataForHash)
+    console.log(hash.digest('hex'))
 };
 
 await calculateHash();
