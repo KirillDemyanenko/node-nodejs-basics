ae9832ba95e31ac36f079fd0c0a2992e20ead078
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-14 17:57:58 +0300

    fix: fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -3,7 +3,7 @@ import { dirname, join} from 'node:path'
 import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const remove = async () => {
-    unlink(join(__dirname, 'files/', 'fileToRemove.txt'), err => {
+    unlink(join(__dirname, 'files', 'fileToRemove.txt'), err => {
         if (err) throw new Error('FS operation failed')
     })
 };
